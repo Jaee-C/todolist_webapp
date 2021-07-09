feb385d7ef7db19bf7193cf0231e47ef81b5cfb3
wengjae <wengjae.1011@gmail.com>
2021-07-09 15:26:29 +0800

    complete login/register/logout backend

--- application.py
+++ application.py
@@ -14,6 +14,8 @@ app = Flask(__name__)
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+db = SQL("sqlite:///todolist.db")
+
 def login_required(f):
     """
     Decorate routes to require login.
@@ -41,10 +43,71 @@ def login():
     # Forget any user_id
     session.clear()
 
-    return render_template("login.html")
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("login.html", user_error="* Please enter username")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return render_template("login.html", pass_error="* Please enter password")
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return render_template("login.html", pass_error="* Password and username do not match")
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register for a new account"""
+    if request.method == "GET":
+        return render_template("register.html")
+
+    # Ensure username was submitted
+    username = request.form.get("username")
+    if not username:
+        return render_template("register.html", user_error="* Please enter a username")
+
+    # Check whether username has been taken
+    user_test = db.execute("SELECT username FROM users WHERE username = ?", username)
+    if len(user_test) != 0:
+        return render_template("register.html", user_error="* Username taken")
+
+    # Check validity of password
+    password = request.form.get("password")
+    confirmation = request.form.get("confirmation")
+    if not password:
+        return render_template("register.html", pass_error="* Please enter a password")
+    elif password != confirmation:
+        return render_template("register.html", conf_error="* Passwords do not match")
+
+    # Add username and password hash into database 'users'
+    db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, generate_password_hash(password))
+    
+    user = db.execute("SELECT id FROM users WHERE username = ?", username)
+    session["user_id"] = user[0]["id"]
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
     # Forget any user_id
-    return render_template("register.html")
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
