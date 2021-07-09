4eb941747305cb699f6e47fc3dfed86541eda556
wengjae <wengjae.1011@gmail.com>
2021-07-09 12:10:23 +0800

    added login and register frontend

--- application.py
+++ application.py
@@ -6,13 +6,45 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from functools import wraps
 from tempfile import mkdtemp
-from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
 # Configure application
 app = Flask(__name__)
 
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
 @app.route("/")
 def index():
     """Show to-do list"""
-    return render_template("index.html")
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        return
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+    # Forget any user_id
+    session.clear()
+
+    return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register for a new account"""
+    # Forget any user_id
+    return render_template("register.html")
